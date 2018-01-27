cb18f0b52668be69ee778aef4fbcc1661321e546
wap4444 <wap4444@mail.ru>
2018-01-28 01:16:40 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -73,7 +73,9 @@ function didOpenRemoteNotificationCallBack(jsonData) {}
            window.plugins.OneSignal
           .startInit("3fc2f01b-b67f-48a5-87bc-bfd1df368503")
           .handleNotificationReceived(didReceiveRemoteNotificationCallBack)
-          .handleNotificationOpened(didOpenRemoteNotificationCallBack)
+          .handleNotificationOpened(function(jsonData) {
+    alert("Notification opened:\n" + JSON.stringify(jsonData));
+  })
               .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.None)
           .iOSSettings(iosSettings)
           .endInit();
