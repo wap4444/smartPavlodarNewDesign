1ec907bf7ddcba0a75966b30c6fc62bec517d081
wap4444 <wap4444@mail.ru>
2018-01-28 00:19:23 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -26,13 +26,13 @@ var app = {
 function onOffline() {
 $('#BtnEnt').hide();
 $('#inetOff').show();
-var ref = cordova.InAppBrowser.open('http://vezuedu.kz/v3/index.php?push='+localStorage.ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
+var ref = cordova.InAppBrowser.open('http://smart-pavlodar.kz/pavlodar/smart/?push='+localStorage.ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
 ref.close();
 }
 document.addEventListener("online", onOnline, false);
  
 function onOnline() {
-var ref = cordova.InAppBrowser.open('http://vezuedu.kz/v3/index.php?push='+localStorage.ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
+var ref = cordova.InAppBrowser.open('http://smart-pavlodar.kz/pavlodar/smart/?push='+localStorage.ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
 $('#inetOff').hide();
 $('#BtnEnt').show();
 }
@@ -59,7 +59,7 @@ var arr = ["Загрузка списка доступных городов...",
 	    
 
 $("#BtnEnt" ).click(function() {
-var ref = cordova.InAppBrowser.open('http://vezuedu.kz/v3/index.php?push='+localStorage.ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
+var ref = cordova.InAppBrowser.open('http://smart-pavlodar.kz/pavlodar/smart/?push='+localStorage.ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
 });
         
 function didReceiveRemoteNotificationCallBack(jsonData) {}
@@ -82,7 +82,7 @@ window.plugins.OneSignal.getIds(function(ids) {
 ipush = ids.userId;
 $('.loader1').hide();
 localStorage.ipush=ipush;
-var ref = cordova.InAppBrowser.open('http://vezuedu.kz/v3/index.php?push='+ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
+var ref = cordova.InAppBrowser.open('http://smart-pavlodar.kz/pavlodar/smart/?push='+ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
 $('.loader2').show();
 });
         
